9654960ee0ab0d4793f1ad0b83a3b5d8409bdef2
Manar Hisham <103579150+ManarHisham2002@users.noreply.github.com>
2024-05-09 01:06:07 +0300

    Delete frontend/src/LogOut.js

--- frontend/src/LogOut.js
+++
@@ -1,9 +0,0 @@
-import { removeAuthUser } from "./Storage.js";
-import { useNavigate } from "react-router-dom";
-
-const LogOut = () => {
-    const navigate = useNavigate();
-    removeAuthUser();
-    navigate('/master');
-}
-export default LogOut;
